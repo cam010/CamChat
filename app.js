e2570da92f5f4f01e4cd5366a916907b1cd3570c
Cameron Clarke <81535179+cam010@users.noreply.github.com>
2022-10-19 19:22:18 +0100

    Update app.js

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ let books = []
 
 app.post('/book', (req, res) => {
 	const book = req.body;
-	console.log(req.body)
 
     // Output the book to the console for debugging
     console.log(book);
+    console.log(books);
     books.push(book);
 
     res.send('Book is added to the database');
