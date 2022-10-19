a626dca96690427b8fbd5e3ccc0aff62363de1f3
Cameron Clarke <81535179+cam010@users.noreply.github.com>
2022-10-19 17:42:23 +0100

    Sample GET request/response

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3030;
 
-// your code
+app.get('/', (req, res) => {
+    res.send('Hello World, from express');
+});
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
