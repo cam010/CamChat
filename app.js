5f4f6bbcb1ea3c8fb1c0da35d103f207de279e75
Cameron Clarke <81535179+cam010@users.noreply.github.com>
2022-10-19 18:54:22 +0100

    Sample Post Request

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const app = express();
 const PORT = process.env.PORT || 3030;
 
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
+});
+
+let books = []
+
+app.post('/book', (req, res) => {
+    const book = req.body;
+
+    // Output the book to the console for debugging
+    console.log(book);
+    books.push(book);
+
+    res.send('Book is added to the database');
 });
 
 app.listen(PORT, () => {
