fe09aeb105b02ff6266d1c9a951cb01c1435abc6
Cameron Clarke <81535179+cam010@users.noreply.github.com>
2022-10-19 11:35:17 +0100

    Made express server

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const http = require('http')
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
 
-const host = 'localhost'
-const port = 8000;
+// your code
 
-const requestListener = function (req, res) {
-	res.writeHead(200)
-	res.end("Server Response!!")
-}
-
-const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-	console.log("Server running")
-})
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
